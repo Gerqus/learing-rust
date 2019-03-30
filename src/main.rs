66af54a005656a9c6ed4ccf6066dc7e154185431
pawel <ppieniacki@gmail.com>
2019-03-30 20:22:21 +0100

    getting number from user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world");
+    let degrees: i32 = loop {
+        let mut degrees = String::new();
+        
+        println!("Please enter number of degrees");
+        io::stdin().read_line(&mut degrees)
+            .expect("Could not read input");
+
+        match degrees.trim().parse() {
+            Ok(number) => break number,
+            Err(_) => continue
+        }
+    };
+
+    println!("You entered {}", degrees);
 }
