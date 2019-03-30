ba8ea5ac2ecc3adb71da68c4ab8ed682bdfcee27
pawel <ppieniacki@gmail.com>
2019-03-30 20:56:39 +0100

    added conversion and proper output

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 fn main() {
-    let degrees: i32 = loop {
+    let degrees: f32 = loop {
         let mut degrees = String::new();
         
         println!("Please enter number of degrees");
@@ -15,19 +15,37 @@ fn main() {
         }
     };
 
-    let unit: String = loop {
+    let unit: char = loop {
         let mut unit = String::new();
         
         println!("Please enter input degrees unit: c - Celsius or f - Farenheit");
 
         io::stdin().read_line(&mut unit)
             .expect("Could not read input");
-        unit = String::from(unit.trim());
+        let unit = unit.trim().chars().next().unwrap();
         
-        if unit == "c" || unit == "f" {
+        if unit == 'c' || unit == 'f' {
             break unit;
         }
     };
 
-    println!("You entered {}*{}", degrees, unit);
+    match unit {
+        'c' => {
+            println!("You entered {}*C", degrees);
+            println!("It is {}F", convert_to_fahrenheit(degrees));
+        },
+        'f' => {
+            println!("You entered {}F", degrees);
+            println!("It is {}*C", convert_to_celsius(degrees));
+        },
+        _ => panic!("Wrong unit after check. Weird stuff happens... o.O"),
+    };
+}
+
+fn convert_to_fahrenheit(degrees: f32) -> f32 {
+    degrees * 1.8 + 32.0
+}
+
+fn convert_to_celsius(degrees: f32) -> f32 {
+    (degrees - 32.0) / 1.8
 }
