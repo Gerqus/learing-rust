80a5b97714343773336ca19d4cbf4821252e2428
pawel <ppieniacki@gmail.com>
2019-03-30 20:35:06 +0100

    getting unit input

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
         let mut degrees = String::new();
         
         println!("Please enter number of degrees");
+
         io::stdin().read_line(&mut degrees)
             .expect("Could not read input");
 
@@ -14,5 +15,19 @@ fn main() {
         }
     };
 
-    println!("You entered {}", degrees);
+    let unit: String = loop {
+        let mut unit = String::new();
+        
+        println!("Please enter input degrees unit: c - Celsius or f - Farenheit");
+
+        io::stdin().read_line(&mut unit)
+            .expect("Could not read input");
+        unit = String::from(unit.trim());
+        
+        if unit == "c" || unit == "f" {
+            break unit;
+        }
+    };
+
+    println!("You entered {}*{}", degrees, unit);
 }
