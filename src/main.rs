838666db35df494112853614d8126f64bfe98760
ppieniacki <ppien@softserveinc.com>
2019-04-01 09:37:56 +0200

    tweaks

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 use std::io;
 
 fn main() {
-    let mut which_fibonacci_number: u16;
-
-    loop {
+    let which_fibonacci_number: u8 = loop {
         let mut which_fibonacci_input = String::new();
         println!("Which subsequent Fibonacci number do you want to know?");
 
         io::stdin().read_line(&mut which_fibonacci_input)
             .expect("Couldn't read from stdin");
 
-        which_fibonacci_number = match which_fibonacci_input.trim().parse() {
+        match which_fibonacci_input.trim().parse() {
             Ok(n) => {
                 break n
             },
@@ -18,15 +16,15 @@ fn main() {
         };
     };
 
-    println!("{}th Fibonacci number is equal to {}", which_fibonacci_number, getFibonacci(which_fibonacci_number));
+    println!("{}th Fibonacci number is equal to {}", which_fibonacci_number, get_fibonacci(which_fibonacci_number));
 }
 
-fn getFibonacci(count: u16) -> u32 {
+fn get_fibonacci(count: u8) -> u64 {
     if count == 0 {
         0
     } else if count == 1 {
         1
     } else {
-        getFibonacci(count - 2) + getFibonacci(count -1)
+        get_fibonacci(count - 2) + get_fibonacci(count -1)
     }
 }
